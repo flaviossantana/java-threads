d63fed5543a7338b081df2236474f62a39c91af9
Flávio Santana <9090flavio@gmail.com>
2020-08-25 18:38:25 -0300

    [THREADS EM JAVA 1: PROGRAMAÇÃO PARALELA][INTRODUÇÃO À THREADS][CRIANDO A PRIMEIRA THREAD]

---
+++ src/main/java/br/com/alura/thread/CauculoThread.java
@@ -0,0 +1,39 @@
+package br.com.alura.thread;
+
+import javax.swing.*;
+import java.math.BigInteger;
+
+public class CauculoThread implements Runnable {
+
+    private JTextField primeiro;
+    private JTextField segundo;
+    private JLabel resultado;
+
+    public CauculoThread(JTextField primeiro, JTextField segundo, JLabel resultado) {
+        this.primeiro = primeiro;
+        this.segundo = segundo;
+        this.resultado = resultado;
+    }
+
+    public void run() {
+
+        System.out.println("CalculoThread.run()");
+
+        long valor1 = Long.parseLong(primeiro.getText());
+        long valor2 = Long.parseLong(segundo.getText());
+        BigInteger calculo = new BigInteger("0");
+
+        for (int i = 0; i < valor1; i++) {
+            System.out.println("VALOR01: " + i);
+            for (int j = 0; j < valor2; j++) {
+                System.out.println("VALOR02: " + j);
+                calculo = calculo.add(new BigInteger("1"));
+                System.out.println("CALCULO: " + calculo);
+            }
+        }
+
+        resultado.setText(calculo.toString());
+
+    }
+
+}
---
+++ src/main/java/br/com/alura/view/AcaoBotao.java
@@ -0,0 +1,30 @@
+package br.com.alura.view;
+
+import br.com.alura.thread.CauculoThread;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AcaoBotao implements ActionListener {
+
+    private JTextField primeiro;
+    private JTextField segundo;
+    private JLabel resultado;
+
+    public AcaoBotao(JTextField primeiro, JTextField segundo, JLabel resultado) {
+        this.primeiro = primeiro;
+        this.segundo = segundo;
+        this.resultado = resultado;
+    }
+
+
+    public void actionPerformed(ActionEvent e) {
+        Runnable cauculoThread = new CauculoThread(primeiro, segundo, resultado);
+        Thread thread = new Thread(cauculoThread, CauculoThread.class.getSimpleName());
+        thread.start();
+        resultado.setText("Aguarde!");
+        System.out.println("thread.start()");
+    }
+
+}
